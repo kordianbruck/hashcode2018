afad745d42e615b1b8a38d8a14d33706b630fb34
pfent <philipp@fent.de>
2018-03-01 19:04:30 +0100

    read rides

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,38 @@
-import java.util.*;
-import java.io.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+        String filename = "indata/a_example.in";
+        Scanner in = new Scanner(new BufferedReader(new FileReader(filename)));
 
-        int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
+        int rows = in.nextInt();
+        int cols = in.nextInt();
+        int vehicles = in.nextInt();
+        int rides = in.nextInt();
+        int bonus = in.nextInt();
+        int steps = in.nextInt();
 
-        for (int i = 1; i <= t; ++i) {
-            int n = in.nextInt();
-            int m = in.nextInt();
-            System.out.println("Case #" + i + ": " + (n + m) + " " + (n * m));
+        for (int i = 1; i <= rides; ++i) {
+            Ride ride = readRide(in);
+            System.out.println(ride);
         }
+    }
+
+    @NotNull
+    private static Ride readRide(Scanner in) {
+        int startRow = in.nextInt();
+        int startCol = in.nextInt();
+        int finRow = in.nextInt();
+        int finCol = in.nextInt();
+        int earliestStart = in.nextInt();
+        int latestFinish = in.nextInt();
+        return new Ride(new Location(startRow, startCol), new Location(finRow, finCol), earliestStart, latestFinish);
     }
 
 
