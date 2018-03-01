1f120369fe2a433a5d112a2c6e5ec210d6b3b0fb
Kordian Bruck <k@bruck.me>
2018-03-01 18:22:14 +0100

    Init Java project to be runnable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
         int t = in.nextInt();  // Scanner has functions to read ints, longs, strings, chars, etc.
+
         for (int i = 1; i <= t; ++i) {
             int n = in.nextInt();
             int m = in.nextInt();
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class MainTest {
+
+
+    @Test
+    public void testSomething() {
+        assertTrue(true);
+    }
+}
